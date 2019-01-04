b816d3ccdca906d8e0820b0ba9dd925e92456a3d
Kyle Pu <31393170+Kyle-P@users.noreply.github.com>
2019-01-03 23:06:14 -0800

    Upload demo.m
    demo.m allows users to input new data that the model hasn't seen before. It'll output the model's prediction as well as allow users to input more data or stop the process.

---
+++ demo.m
@@ -0,0 +1,34 @@
+function prediction = demo(Theta1, Theta2)
+  
+  % Default value
+  prediction = "Not found...";
+  
+  % Input a new value for the model to predict on at least once before stopping
+  demoing = 1;
+  
+  while demoing == 1
+    
+    fprintf("-----------------------");
+    
+    fprintf("\nWrite a new number and press a key after saving...");  
+    pause();
+    
+    % Load image, process for classifier, and return prediction
+    originalImage = imread("Demo.png");
+    processedImage = processImage(originalImage);
+    prediction = predict(Theta1, Theta2, processedImage);
+    
+    % Concatenate prediction to the output string after converting from integer to string value
+    fprintf(["\n\nYour number: " mat2str(prediction)]);
+    fprintf("\n");
+    
+    fprintf("\nPress a key to continue...");
+    pause();
+    fprintf("\n");
+    
+    % Returns 1 if "yes" and 0 if "no"
+    demoing = yes_or_no("Do you want to try another one?!?!?");
+    
+  endwhile
+  
+endfunction
