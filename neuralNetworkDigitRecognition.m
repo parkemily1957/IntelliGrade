334241be8edf44a16a7e033e5b3be97a108a1fd2
Kyle Pu <kyle.pu@STCYBERMOBILE08.esuhsd.org>
2019-02-02 22:26:06 -0800

    Updating grading demo...

--- neuralNetworkDigitRecognition.m
+++ neuralNetworkDigitRecognition.m
@@ -130,6 +130,8 @@ fprintf('\nTest Set Accuracy: %f\n', mean(double(pred == testSolutions)) * 100);
 pkg load image;
 
 fprintf("\nPress Enter when you're ready for the demo!!!\n\n");
+
+fprintf("\n--------------------------------------------------\n");
 %demo(Theta1, Theta2, 1000);
 
 gradeFromCam(Theta1, Theta2);
